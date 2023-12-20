df160da89f5798129caa49d82c82d24ab4ac44bb
maazalside <maazalside@gmail.com>
2023-12-20 23:36:41 +0300

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,29 +9,33 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_number;
-	int temp;
+	int temp, swapped;
 
 	if (!array || size < 2)
-        {
-                return;
-        }
+	{
+		return;
+	}
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_number = i;
+		swapped = 0;
 
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_number])
+			{
 				min_number = j;
-
+				swapped = 1;
+			}
 		}
-		if (min_number != i)
+		if (swapped)
 		{
 			temp = array[min_number];
 			array[min_number] = array[i];
 			array[i] = temp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
+
